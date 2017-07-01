21dff3896ed69ab837567b14dad121ade993447e
VILLERS Mickaël <mickael.villers@epitech.eu>
2017-07-01 23:35:05 +0200

    add navigation url

--- resources/assets/js/components/Navigation.js
+++ resources/assets/js/components/Navigation.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { push } from 'react-router-redux';
+import { connect } from 'react-redux';
+
 import { Button, Container, Menu, Search } from 'semantic-ui-react';
 
 const color = 'pink';
 
-export default class NavigationComponent extends React.PureComponent {
+export class NavigationComponent extends React.PureComponent {
   constructor(props) {
     super(props);
 
@@ -14,8 +17,10 @@ export default class NavigationComponent extends React.PureComponent {
     };
   }
 
-  handleItemClick(e, { name }) {
-    this.setState({ activeItem: name });
+  handleItemClick(item, url) {
+    const { moveTo } = this.props;
+    this.setState({ activeItem: item.name });
+    moveTo(url);
   }
 
   render() {
@@ -34,8 +39,8 @@ export default class NavigationComponent extends React.PureComponent {
         style={style}
       >
         <Container>
-          <Menu.Item name="accueil" content="Accueil" active={activeItem === 'accueil'} onClick={this.handleItemClick} />
-          <Menu.Item name="generalite" content="Généralité" active={activeItem === 'generalite'} onClick={this.handleItemClick} />
+          <Menu.Item name="accueil" content="Accueil" active={activeItem === 'accueil'} onClick={(e, name) => this.handleItemClick(name, '/')} />
+          <Menu.Item name="generalite" content="Généralité" active={activeItem === 'generalite'} onClick={(e, name) => this.handleItemClick(name, '/')} />
 
           <Menu.Menu position="right">
             <Menu.Item>
@@ -57,4 +62,14 @@ export default class NavigationComponent extends React.PureComponent {
 
 NavigationComponent.propTypes = {
   style: PropTypes.shape({}).isRequired,
+  moveTo: PropTypes.func.isRequired,
 };
+
+const mapStateToProps = () => ({
+});
+
+const mapDispatchToProps = dispatch => ({
+  moveTo: url => dispatch(push(url)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavigationComponent);
